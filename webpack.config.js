f83c809cc462c8cf411cae1071345d1a1db13a20
Dau Trong Hoang <45507664+sakkarose@users.noreply.github.com>
2025-02-01 17:32:39 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,21 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "events": require.resolve("events/"),
+      "path": require.resolve("path-browserify"),
+      "fs": false,
+      "child_process": false,
+      "worker_threads": false,
+      "timers": require.resolve("timers-browserify"),
+      "https": require.resolve("https-browserify"),
+      "process": require.resolve("process/browser"),
+    },
+  },
+  mode: 'production',
 };
